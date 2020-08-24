fda8e72a65d03af75e88733ec60f279271e7a430
leewonz <lwk530@gmail.com>
2020-08-24 11:55:54 +0900

    CartDAO.addCart 메소드 생성

--- src/main/java/elecshopping/CartDAO.java
+++ src/main/java/elecshopping/CartDAO.java
@@ -1,5 +1,30 @@
 package elecshopping;
 
-public class CartDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+public class CartDAO {
+	public CartVO addCart(String id, int pnum) {
+
+		String sql = "INSERT into cart (id, pnum)"
+				+ " values(?, ?)";
+		Connection con = null;
+		PreparedStatement ps = null;
+		int row = 0;
+		try {
+			//con = JDBCUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			
+			//?세팅작업
+			ps.setString(1,  id);
+			ps.setInt(2,  pnum);
+			row = ps.executeUpdate();
+			
+		}catch (Exception e) {
+			System.out.println(e.toString());
+		}finally {
+			//JDBCUtil.close(con, ps, null);
+		}
+		return null;
+	}
 }
